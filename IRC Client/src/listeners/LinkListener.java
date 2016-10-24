43b7f85787ad88844aab7ea26ae842dce7790837
Dallan Healey <dallan.healey@live.com>
2016-10-23 22:22:46 -0500

    Not sure

--- IRC Client/src/listeners/LinkListener.java
+++ IRC Client/src/listeners/LinkListener.java
@@ -12,13 +12,20 @@ public class LinkListener extends MouseAdapter
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{
-		System.out.println("Mouse coords: " + e.getX() + ", " + e.getY());
+		System.out.println("Mouse coordinates: " + e.getX() + ", " + e.getY() + "\nCaret: " + Client.messages.getCaretPosition());
+
 		try
 		{
 			if (Desktop.isDesktopSupported())
 			{
 				if ((Client.links.get(Client.messages.getCaretPosition()) != null) && (e.getY() >= Client.messages.getCaretPosition() && e.getY() <= Client.messages.getCaretPosition() + 21))
+				{
 					Desktop.getDesktop().browse(Client.links.get(Client.messages.getCaretPosition()));
+				}
+				else
+				{
+					System.out.println("Did bnot wiork");
+				}
 			}
 			else
 				System.out.println("Desktop Not Supported");
